9d96cccbdc97234b66032283c20531264c140af5
Esteban Barroso <estebanjose1609@gmail.com>
2026-08-13 16:42:29 -0300

    initial commit
    estructura base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Inmobiliaria_BarrosoEsteban.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
