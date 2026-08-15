7e7266ad329a2abcabe4fc14f15a89a160a49495
josemir01 <josemirzaleh45@gmail.com>
2026-08-15 19:14:19 -0300

    inquilino controller creado

---
+++ Controllers/InquilinosController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Inmobiliaria_.Net_Core.Models;
+
+namespace Inmobiliaria_.Net_Core.Controllers
+{
+    public class InquilinosController : Controller
+    {
+
+    }
+}
