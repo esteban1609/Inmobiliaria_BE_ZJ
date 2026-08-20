2f115093e37e76ccdf96bfbdab719448e369c972
josemir01 <josemirzaleh45@gmail.com>
2026-08-20 17:43:19 -0300

    ABM de Inquilino y sus vistas

--- Controllers/InquilinosController.cs
+++ Controllers/InquilinosController.cs
@@ -5,6 +5,27 @@ namespace Inmobiliaria_.Net_Core.Controllers
 {
     public class InquilinosController : Controller
     {
+        private readonly IRepositorioInquilino repositorio;
 
+        public InquilinosController(IRepositorioInquilino repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Inquilino inquilino)
+        {
+            if (ModelState.IsValid)
+            {
+                repositorio.alta(inquilino);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(inquilino);
+        }
     }
 }
